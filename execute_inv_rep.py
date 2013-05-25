13ce4cabaa36acb6b11b2e778916000200f3a629
Lex Nederbragt <lex.nederbragt@bio.uio.no>
2013-05-25 19:30:21 +0200

    Added argument parsing + some help text. Accepts one argument, the input filename

--- execute_inv_rep.py
+++ execute_inv_rep.py
@@ -14,19 +14,25 @@ except:
 
 # <codecell>
 
-parser = argparse.ArgumentParser(description='Help text to be added.')
+desc = ("""Strips off the 3' copy of any inverted repeat.\n
+Input: one fastq file\n
+Output:
+1) a new fastq file with cleaned sequences
+'infile.fastq' generates 'infile.clean.fastq'
+2) a file called infile.inv_reps.txt with the stripped sequences and their counts.
+3) a file called infile.inv_rep_lengths.txt with the length distribution of the stripped sequences.
+""")
+
+# <codecell>
+
+parser = argparse.ArgumentParser(description=desc)
 parser.add_argument('-i','--input', help='Input file name',required=True)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     ## show values ##
     print ("Input file: %s" % args.input )
-    print ("Output file: %s" % args.output )
-
-# <codecell>
-
-infile = '/Users/alexajo/Dropbox/current/inverted_repeats/data/Subset_Lex.fastq'
-ir.process(infile)
+    ir.process(args.input)
 
 # <codecell>
 
