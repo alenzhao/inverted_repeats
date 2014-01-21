6802d41de18f686ec5b9bdc487a1fd8e72f45c4c
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2014-01-21 08:54:55 +0100

    Initial commit get_from_sra and get_from_ensembl scripts

---
+++ get_from_ensembl.py
@@ -0,0 +1,14 @@
+# adjusted from 
+# https://raw.github.com/nickloman/benchtop-sequencing-comparison/master/scripts/get_from_sra.py
+
+import os
+import sys
+
+fullfn = sys.argv[1]
+[species, asm, release] = fullfn.split('.')[0:3]
+
+ftp_path = 'ftp://ftp.ensembl.org/pub/release-%s/fasta/%s/dna/%s' % (release, species.lower(), fullfn)
+
+cmd = "curl -s -O %s" % ftp_path
+print cmd
+os.system(cmd)
---
+++ get_from_sra.py
@@ -0,0 +1,13 @@
+# adjusted from 
+# https://raw.github.com/nickloman/benchtop-sequencing-comparison/master/scripts/get_from_sra.py
+
+import os
+import sys
+
+fullfn = sys.argv[1]
+dir = fullfn[0:6]
+fn = fullfn[0:9]
+
+cmd = "curl -s -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/%s/%s/%s.fastq.gz" % (dir, fn, fullfn)
+print cmd
+os.system(cmd)
