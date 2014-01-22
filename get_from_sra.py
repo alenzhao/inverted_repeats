fa4ddb953b359c5fc079455cff0ca7c25731968b
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2014-01-22 10:13:45 +0100

    Added -S flag t curl command so that it outputs error-messages

--- get_from_sra.py
+++ get_from_sra.py
@@ -8,6 +8,6 @@ fullfn = sys.argv[1]
 dir = fullfn[0:6]
 fn = fullfn[0:9]
 
-cmd = "curl -s -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/%s/%s/%s.fastq.gz" % (dir, fn, fullfn)
+cmd = "curl -s -S -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/%s/%s/%s.fastq.gz" % (dir, fn, fullfn)
 print cmd
 os.system(cmd)
